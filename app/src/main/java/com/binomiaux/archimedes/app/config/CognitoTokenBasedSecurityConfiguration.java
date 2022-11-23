4d90619d19366a36403f6a37751976aa3d44b505
Oscar Fimbres <ofimbres@MacBook-Pro.local>
2022-11-23 13:52:56 -0800

    Upgrade gradle from 6.8 to 7.2, add missing dependencies into gradle

--- app/src/main/java/com/binomiaux/archimedes/app/config/CognitoTokenBasedSecurityConfiguration.java
+++ app/src/main/java/com/binomiaux/archimedes/app/config/CognitoTokenBasedSecurityConfiguration.java
@@ -5,11 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
 
 @Slf4j
 @Configuration
+@EnableWebSecurity
 @Order(1)
 public class CognitoTokenBasedSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
