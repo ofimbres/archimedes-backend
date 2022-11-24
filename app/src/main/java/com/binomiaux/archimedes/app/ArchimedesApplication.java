99605e9dffa8746595e8f906a2fa852ba3747349
Oscar Fimbres <ofimbres@MacBook-Pro.local>
2022-11-24 11:27:53 -0800

    Add explicit Spring Boot Component Scans

--- app/src/main/java/com/binomiaux/archimedes/app/ArchimedesApplication.java
+++ app/src/main/java/com/binomiaux/archimedes/app/ArchimedesApplication.java
@@ -1,20 +1,22 @@
 package com.binomiaux.archimedes.app;
 
 import com.binomiaux.archimedes.app.config.WebConfigProperties;
+import com.binomiaux.archimedes.repository.ArchimedesRepositoryScan;
+import com.binomiaux.archimedes.service.ArchimedesServiceScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @SpringBootApplication
-@ComponentScan({ "com.binomiaux.archimedes.app", "com.binomiaux.archimedes.service", "com.binomiaux.archimedes.repository" })
+@Import(value = { ArchimedesServiceScan.class, ArchimedesRepositoryScan.class })
 @EnableAutoConfiguration(exclude={SecurityAutoConfiguration.class})
 @EnableConfigurationProperties({WebConfigProperties.class})
 public class ArchimedesApplication {
@@ -35,11 +37,11 @@ public class ArchimedesApplication {
 			public void addCorsMappings(CorsRegistry registry) {
 				WebConfigProperties.Cors cors = webConfigProperties.getCors();
 				registry.addMapping("/**")
-						.allowedOrigins(cors.getAllowedOrigins()
-						/*.allowedMethods(cors.getAllowedMethods())
+						.allowedOrigins(cors.getAllowedOrigins())
+						.allowedMethods(cors.getAllowedMethods())
 						.maxAge(cors.getMaxAge())
 						.allowedHeaders(cors.getAllowedHeaders())
-						.exposedHeaders(cors.getExposedHeaders()*/);
+						.exposedHeaders(cors.getExposedHeaders());
 			}
 		};
 	}
