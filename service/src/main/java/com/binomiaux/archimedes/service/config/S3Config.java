c8ced6daeb2d229c467271524ba62eec5da5b6f8
Oscar Fimbres <oscar.fimbres@gmail.com>
2025-02-09 08:07:54 +0000

    Update configs to use default set of aws credentials

--- service/src/main/java/com/binomiaux/archimedes/service/config/S3Config.java
+++ service/src/main/java/com/binomiaux/archimedes/service/config/S3Config.java
@@ -2,9 +2,8 @@ package com.binomiaux.archimedes.service.config;
 
 import com.binomiaux.archimedes.service.awsservices.S3Service;
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -16,19 +15,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
-
     @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
     public S3Client s3Client() {
-        AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
-                AwsBasicCredentials.create(accessKey, secretKey));
+        AwsCredentialsProvider credentialsProvider = DefaultCredentialsProvider.create();
 
         return S3Client.builder()
                 .region(Region.of(region))
@@ -38,8 +30,7 @@ public class S3Config {
 
     @Bean
     public S3Presigner s3Presigner() {
-        AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
-                AwsBasicCredentials.create(accessKey, secretKey));
+        AwsCredentialsProvider credentialsProvider = DefaultCredentialsProvider.create();
 
         return S3Presigner.builder()
                 .region(Region.of(region))
