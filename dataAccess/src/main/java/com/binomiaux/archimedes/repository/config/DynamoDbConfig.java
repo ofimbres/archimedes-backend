64c7f3515b2dc5b089340327f089e688fadffaa5
Oscar Fimbres <oscar.fimbres@gmail.com>
2025-02-09 07:59:57 +0000

    Update definition and credentials

--- dataAccess/src/main/java/com/binomiaux/archimedes/repository/config/DynamoDbConfig.java
+++ dataAccess/src/main/java/com/binomiaux/archimedes/repository/config/DynamoDbConfig.java
@@ -1,27 +1,19 @@
 package com.binomiaux.archimedes.repository.config;
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DynamoDbConfig {
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
 
     @Bean
     public DynamoDbClient dynamoDbClient() {
-        AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
-                AwsBasicCredentials.create(accessKey, secretKey));
+        AwsCredentialsProvider credentialsProvider = DefaultCredentialsProvider.create();
 
         DynamoDbClient standardClient = DynamoDbClient.builder()
                 .credentialsProvider(credentialsProvider)
@@ -33,8 +25,7 @@ public class DynamoDbConfig {
 
     @Bean
     public DynamoDbEnhancedClient dynamoDbEnhancedClient() {
-        AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
-                AwsBasicCredentials.create(accessKey, secretKey));
+        AwsCredentialsProvider credentialsProvider = DefaultCredentialsProvider.create();
 
         DynamoDbClient standardClient = DynamoDbClient.builder()
                 .credentialsProvider(credentialsProvider)
